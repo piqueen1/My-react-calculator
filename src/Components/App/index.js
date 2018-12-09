155e0ef7acd6e4536e81de3d2bcbfd9bd657f259
Finn Thye <piqueen@gmail.com>
2018-12-09 09:49:22 -0700

    Complete functionality; begin styling

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,50 +1,97 @@
 import React, { Component } from 'react';
 import './styles.css';
-import Display from '../Display';
-import Buttons from '../Buttons';
-import Button from '../Button';
-import ClearButton from '../ClearButton';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.operators = ['/', '*', '+', '-'];
+
+    this.digits = ['1','2','3','4','5','6','7','8','9','0'];
+
     this.state = {
-      operations: []
+      steps: []
     };
   }
 
-  calculateOperations(props) {
+  pushOperator = (operator) => {
+    return () => {
+      // if there are no steps, ignore operator unless it's a -
+      if (this.state.steps.length === 0 && operator !== '-') {
+        return;
+      }
 
+      // if the prior value is an operator, replace that value with current operator
+      const newSteps = this.state.steps.slice(0);
+      const lastStep = newSteps.slice(-1)[0];
+
+      if (this.operators.includes(lastStep)) {
+        console.log('replacing lastStep:',lastStep);
+        newSteps.splice(-1, 1, operator)
+        this.setState({
+          steps: newSteps,
+        })
+        return;
+      }
+
+      newSteps.push(operator)
+      this.setState({
+        steps: newSteps,
+      })
+    }
+  }
+
+  pushNumber = (num) => {
+    return () => {
+      this.state.steps.push(num)
+      this.setState({
+        steps: this.state.steps,
+      })
+    }
+  }
+
+  clearSteps = () => {
+    this.setState({
+      steps: [],
+    })
+  }
+
+  evaluate = () => {
+    const result = eval(this.state.steps.join(''));
+    this.setState({
+      steps: [result]
+    })
   }
 
   render() {
     return (
-      <div className="App">
-      Hi there
-        {/* {this.props.children} */}
-        <Display />
-        <Buttons>
-          <Button onclick={this.calculateOperations()} value="7" label="7" />
-          <Button onclick={this.calculateOperations()} value="8" label="8" />
-          <Button onclick={this.calculateOperations()} value="9" label="9" />
-          <Button onclick={this.calculateOperations()} value="/" label="/" />
+      <div className="app">
+        {/* Display */}
+        <div className="display">
+          {this.state.steps.join('')}
+        </div>
 
-          <Button onclick={this.calculateOperations()} value="4" label="4" />
-          <Button onclick={this.calculateOperations()} value="5" label="5" />
-          <Button onclick={this.calculateOperations()} value="6" label="6" />
-          <Button onclick={this.calculateOperations()} value="x" label="x" />
+        <div className="middle">
+          {/* Numbers */}
+          <div className="digits">
+            {this.digits.map((digit) => (
+              <button key={digit} onClick={this.pushNumber(digit)}>{digit}</button>
+            ))}
+          </div>
 
-          <Button onclick={this.calculateOperations()} value="1" label="1" />
-          <Button onclick={this.calculateOperations()} value="2" label="2" />
-          <Button onclick={this.calculateOperations()} value="3" label="3" />
-          <Button onclick={this.calculateOperations()} value="-" label="-" />
+          {/* Operators */}
+          <div className="operators">
+            {this.operators.map((operator) => (
+              <button key={operator} onClick={this.pushOperator(operator)}>{operator}</button>
+            ))}
+          </div>
+        </div>
 
-          <Button onclick={this.calculateOperations()} value="0" label="0" />
-          <Button onclick={this.calculateOperations()} value="." label="." />
-          <Button onclick={this.calculateOperations()} value="=" label="=" />
-          <Button onclick={this.calculateOperations()} value="+" label="+" />          <Button onclick={this.calculateOperations()} value="clear" label="Clear" />
-        </Buttons>
-        <ClearButton />
+        {/* Actions */}
+        <div className="actions">
+          <button onClick={this.clearSteps}>Clear</button>
+          <button onClick={this.evaluate}>=</button>
+        </div>
       </div>
     );
   }
--- src/Components/Buttons/index.js
+++ src/Components/Buttons/index.js
@@ -3,11 +3,36 @@ import './styles.css';
 import Button from '../Button';
 
 class Buttons extends Component {
+  calculateOperations(props) {
+    /* if...
+     it's a number or operation: add it to the array that will become the eval string
+     it's clear: make state = []
+     it's equals: join state [] into string and use math.eval to display answer */
+  }
 
   render() {
     return (
       <div className="Buttons">
-        {this.props.children}
+        <Button onclick={this.calculateOperations()} value="7" label="7" />
+        <Button onclick={this.calculateOperations()} value="8" label="8" />
+        <Button onclick={this.calculateOperations()} value="9" label="9" />
+        <Button onclick={this.calculateOperations()} value="/" label="/" />
+
+        <Button onclick={this.calculateOperations()} value="4" label="4" />
+        <Button onclick={this.calculateOperations()} value="5" label="5" />
+        <Button onclick={this.calculateOperations()} value="6" label="6" />
+        <Button onclick={this.calculateOperations()} value="x" label="x" />
+
+        <Button onclick={this.calculateOperations()} value="1" label="1" />
+        <Button onclick={this.calculateOperations()} value="2" label="2" />
+        <Button onclick={this.calculateOperations()} value="3" label="3" />
+        <Button onclick={this.calculateOperations()} value="-" label="-" />
+
+        <Button onclick={this.calculateOperations()} value="0" label="0" />
+        <Button onclick={this.calculateOperations()} value="." label="." />
+        <Button onclick={this.calculateOperations()} value="=" label="equals" />
+        <Button onclick={this.calculateOperations()} value="+" label="+" />
+        <Button onclick={this.calculateOperations()} value="clear" label="Clear" />
       </div>
       );
   }
